69fb3af78fe26656310185bdbd569b50ec350688
LLath <rothpatrick12@yahoo.de>
2022-05-12 13:40:41 +0200

    fix: breaking BTTV & 7TV

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ document.addEventListener("getPng", e => {
 const buildBadge = (added_node) => {
   const usernameContainer = added_node.getElementsByClassName("chat-line__username-container")[0]
   if (usernameContainer === undefined) {
-    // console.log("UsernameContainer is empty", usernameContainer)
     return
   }
   const badges = usernameContainer.querySelector("span")
@@ -38,7 +37,6 @@ const buildBadge = (added_node) => {
     const username = usernameContainer.getElementsByClassName("chat-author__display-name")[0].innerHTML
     const msg = `!so ${username}`
 
-    setInputValue(msg)
     getInputController().props.onSendMessage(msg)
   })
 }
@@ -54,8 +52,7 @@ window.addEventListener("load", event => {
   const callback = (mutationList, observer) => {
     for (const mutation of mutationList) {
       mutation.addedNodes.forEach(added_node => {
-        // console.log("ADDED NODE", added_node)
-        if (added_node.classList.contains("chat-line__message")) {
+        if (added_node.classList?.contains("chat-line__message")) {
           buildBadge(added_node)
         }
       })
@@ -68,29 +65,8 @@ window.addEventListener("load", event => {
 
 })
 
-
 //#region Event helpers
 // https://github.com/SevenTV/SevenTV/blob/dev/src/Sites/twitch.tv/Util/Twitch.ts
-function setInputValue(value) {
-  value = value.replace(/�/g, ''); // omit weird character
-  const el = document.querySelector("[data-a-target='chat-input']");
-  if (el && typeof el.value != undefined) {
-    el.value = value;
-    el.dispatchEvent(new Event('input', { bubbles: true }));
-
-    const inst = getReactInstance(el);
-
-    if (inst) {
-      const props = inst.memoizedProps;
-      if (props && props.onChange) {
-        props.onChange({ target: el });
-      }
-    }
-  } else {
-    const el = getChatInput()?.props;
-    el.onChange({ target: { value: value } });
-  }
-}
 
 function findReactParents(node, predicate, maxDepth = 15, depth = 0) {
   let success = false;
